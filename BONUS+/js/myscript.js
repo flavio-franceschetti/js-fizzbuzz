c4b346e029ed340c018271258b7f76ecf66566b3
flavio-franceschetti <flaviofranceschetti98@gmail.com>
2024-06-06 18:49:32 +0200

    feat: aggiunda dello style sul css e delle classi con i colori alle varie condizioni

--- BONUS+/js/myscript.js
+++ BONUS+/js/myscript.js
@@ -6,22 +6,37 @@
 // Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
 // Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo “a mano”
 
+const numberList = document.querySelector(".number-list");
+
 //creo un ciclo che mi stampa i numeri da 0 a 100 in console
 for (let i = 0; i <= 100; i++) {
+  //creo l'elemento li
+  const listItem = document.createElement("li");
   //creo la condizione che se il numero è multiplo di 5 e 3 viene stampato "FizzBuzz"
   if (i % 3 === 0 && i % 5 === 0) {
-    console.log("FizzBuzz");
-
+    //assegno a list item fizzbuzz
+    listItem.textContent = "FizzBuzz";
+    //aggiungo la classe con il colore a questa condizione
+    listItem.classList.add("fizzbuzz");
     //creo la condizione che se il numero è multiplo di 5 viene stampato "buzz"
   } else if (i % 5 === 0) {
-    console.log("buzz");
+    //assegno a list item buzz
+    listItem.textContent = "Buzz";
+    //aggiungo la classe con il colore a questa condizione
+    listItem.classList.add("buzz");
 
     //creo la condizione che se il numero è multiplo di 3 viene stampato "fizz"
   } else if (i % 3 === 0) {
-    console.log("fizz");
+    //assegno a list item fizz
+    listItem.textContent = "Fizz";
+    //aggiungo la classe con il colore a questa condizione
+    listItem.classList.add("fizz");
 
     // altrimenti viene stampato il numero normale
   } else {
-    console.log(i);
+    //assegno a list item "i"
+    listItem.textContent = i;
   }
+  //aggiungo i listItem alla numberList
+  numberList.appendChild(listItem);
 }
